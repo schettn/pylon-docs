33fc1de1b281f9242b2a42eca04511bf3eb46f95
Nico Schett <nicoschett@icloud.com>
2024-08-06 14:11:30 +0200

    update icons in features component

--- components/features.tsx
+++ components/features.tsx
@@ -25,35 +25,186 @@ export function Feature({ text, icon }) {
 const FEATURES_LIST = [
   {
     key: "realtimeSchema",
-    icon: <Icon icon="lightbulb" className="w-[20px]" />,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="icon icon-tabler icons-tabler-outline icon-tabler-chart-bar"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M3 13a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v6a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" />
+        <path d="M15 9a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v10a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" />
+        <path d="M9 5a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" />
+        <path d="M4 20h14" />
+      </svg>
+    ),
   },
   {
     key: "typeSafety",
-    icon: <Icon icon="lightbulb" className="w-[20px]" />,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="icon icon-tabler icons-tabler-outline icon-tabler-checks"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M7 12l5 5l10 -10" />
+        <path d="M2 12l5 5m5 -5l5 -5" />
+      </svg>
+    ),
   },
   {
     key: "authentication",
-    icon: <Icon icon="lightbulb" className="w-[20px]" />,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="icon icon-tabler icons-tabler-outline icon-tabler-lock"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M5 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-6z" />
+        <path d="M11 16a1 1 0 1 0 2 0a1 1 0 0 0 -2 0" />
+        <path d="M8 11v-4a4 4 0 1 1 8 0v4" />
+      </svg>
+    ),
   },
   {
     key: "authorization",
-    icon: <Icon icon="lightbulb" className="w-[20px]" />,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="icon icon-tabler icons-tabler-outline icon-tabler-shield"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M12 3a12 12 0 0 0 8.5 3a12 12 0 0 1 -8.5 15a12 12 0 0 1 -8.5 -15a12 12 0 0 0 8.5 -3" />
+      </svg>
+    ),
   },
   {
     key: "logging",
-    icon: <Icon icon="lightbulb" className="w-[20px]" />,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="icon icon-tabler icons-tabler-outline icon-tabler-clipboard"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" />
+        <path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" />
+      </svg>
+    ),
   },
   {
     key: "errorTracking",
-    icon: <Icon icon="lightbulb" className="w-[20px]" />,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="icon icon-tabler icons-tabler-outline icon-tabler-bug"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M9 9v-1a3 3 0 0 1 6 0v1" />
+        <path d="M8 9h8a6 6 0 0 1 1 3v3a5 5 0 0 1 -10 0v-3a6 6 0 0 1 1 -3" />
+        <path d="M3 13l4 0" />
+        <path d="M17 13l4 0" />
+        <path d="M12 20l0 -6" />
+        <path d="M4 19l3.35 -2" />
+        <path d="M20 19l-3.35 -2" />
+        <path d="M4 7l3.75 2.4" />
+        <path d="M20 7l-3.75 2.4" />
+      </svg>
+    ),
   },
   {
     key: "databaseIntegration",
-    icon: <Icon icon="lightbulb" className="w-[20px]" />,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="icon icon-tabler icons-tabler-outline icon-tabler-tool"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M7 10h3v-3l-3.5 -3.5a6 6 0 0 1 8 8l6 6a2 2 0 0 1 -3 3l-6 -6a6 6 0 0 1 -8 -8l3.5 3.5" />
+      </svg>
+    ),
   },
   {
     key: "optimizedDeployment",
-    icon: <Icon icon="lightbulb" className="w-[20px]" />,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="icon icon-tabler icons-tabler-outline icon-tabler-brand-docker"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M22 12.54c-1.804 -.345 -2.701 -1.08 -3.523 -2.94c-.487 .696 -1.102 1.568 -.92 2.4c.028 .238 -.32 1 -.557 1h-14c0 5.208 3.164 7 6.196 7c4.124 .022 7.828 -1.376 9.854 -5c1.146 -.101 2.296 -1.505 2.95 -2.46z" />
+        <path d="M5 10h3v3h-3z" />
+        <path d="M8 10h3v3h-3z" />
+        <path d="M11 10h3v3h-3z" />
+        <path d="M8 7h3v3h-3z" />
+        <path d="M11 7h3v3h-3z" />
+        <path d="M11 4h3v3h-3z" />
+        <path d="M4.571 18c1.5 0 2.047 -.074 2.958 -.78" />
+        <path d="M10 16l0 .01" />
+      </svg>
+    ),
   },
 ];
 
